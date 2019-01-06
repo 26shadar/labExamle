69c64582f84a24eff5a8676c40feeb3cfe7c0f6b
26shadar <46422637+26shadar@users.noreply.github.com>
2019-01-06 18:13:23 +0200

    Add files via upload

---
+++ BE/Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class Tester
+    {
+        public string  ID { get; set; }
+        public string Family { get; set; }
+        public string PrivateName { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
